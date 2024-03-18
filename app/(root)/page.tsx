1f33af2c8ff6de38fd77eb1ac584b78368d5db84
manish0444 <144358214+manish0444@users.noreply.github.com>
2024-03-18 16:16:48 +0545

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import Collections from "@/components/Collections";
+import ProductList from "@/components/ProductList";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <>
+   <Image src="/fashion.png" alt="banner" width={2000} height={1000} className="w-screen"/>
+   <Collections/>
+   <ProductList/>
+   </>
+  );
+}
