bebf94c139e47cda03436d1c2338fa05f5cc7cd5
manish0444 <144358214+manish0444@users.noreply.github.com>
2024-03-19 17:54:37 +0545

    banner

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <>
-   <Image src="/fashion.png" alt="banner" width={2000} height={1000} className="w-screen"/>
+   <Image src="/banner.png" alt="banner" width={2000} height={1000} className="w-screen"/>
    <Collections/>
    <ProductList/>
    </>
